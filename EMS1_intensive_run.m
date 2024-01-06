57859b1fa6f4c2f354132c5bdeaa69d939dd509a
mycodereposit <126697725+mycodereposit@users.noreply.github.com>
2024-01-07 00:53:14 +0700

    Add files via upload

--- EMS1_intensive_run.m
+++ EMS1_intensive_run.m
@@ -4,10 +4,7 @@ dataset_detail = readtable('dataset/dataset_detail.csv');
 dataset_name = dataset_detail.name;
 
 options = optimoptions('intlinprog','MaxTime',40);
-% high load case : 2023-04-18 - 2023-04-22
-% low  load case : 2023-05-26 - 2023-05-30
-% high solar case: 2023-05-12 - 2023-05-16
-% low  solar case: 2023-04-24 - 2023-04-28
+
 
 %--- user-input parameter ----
 PARAM.Horizon = 4;  % horizon to optimize (day)
@@ -44,25 +41,26 @@ for i = 1:length(dataset_name)
     %parameter part
     
     
-    PARAM.battery.charge_effiency = 0.95; %bes charge eff
-    PARAM.battery.discharge_effiency = 0.95*0.93; %  bes discharge eff note inverter eff 0.93-0.96
-    PARAM.battery.discharge_rate = 30; % kW max discharge rate
-    PARAM.battery.charge_rate = 30; % kW max charge rate
-    PARAM.battery.actual_capacity = 125; % kWh soc_actual_capacity 
-    PARAM.battery.initial = 50; % userdefined int 0-100 %
-    PARAM.battery.min = 20; %min soc userdefined int 0-100 %
-    PARAM.battery.max = 80; %max soc userdefined int 0-100 %
+    PARAM.battery.charge_effiency = [0.95 0.95]; %bes charge eff
+    PARAM.battery.discharge_effiency = [0.95*0.93 0.95*0.93]; %  bes discharge eff note inverter eff 0.93-0.96
+    PARAM.battery.discharge_rate = [50 50]; % kW max discharge rate
+    PARAM.battery.charge_rate = [50 50]; % kW max charge rate
+    PARAM.battery.actual_capacity = [125 125]; % kWh soc_capacity 
+    PARAM.battery.initial = [50 50]; % userdefined int 0-100 %
+    PARAM.battery.min = [20 20]; %min soc userdefined int 0-100 %
+    PARAM.battery.max = [80 80]; %max soc userdefined int 0-100 %
+    PARAM.battery.num_batt = length(PARAM.battery.actual_capacity);
     % end of parameter part
     % optimize var = [Pnet u Pdchg xdchg Pchg xchg soc Pac Xac1 Xac2 Xac3 Xac4]
     
     
     Pnet =      optimvar('Pnet',k,'LowerBound',-inf,'UpperBound',inf);
     u =         optimvar('u',k,'LowerBound',0,'UpperBound',inf);
-    Pdchg =     optimvar('Pdchg',k,2,'LowerBound',0,'UpperBound',inf);
-    xdchg =     optimvar('xdchg',k,2,'LowerBound',0,'UpperBound',1,'Type','integer');
-    Pchg =      optimvar('Pchg',k,2,'LowerBound',0,'UpperBound',inf);
-    xchg =      optimvar('xchg',k,2,'LowerBound',0,'UpperBound',1,'Type','integer');
-    soc =       optimvar('soc',k+1,2,'LowerBound',PARAM.battery.min,'UpperBound',PARAM.battery.max);
+    Pdchg =     optimvar('Pdchg',k,PARAM.battery.num_batt,'LowerBound',0,'UpperBound',inf);
+    xdchg =     optimvar('xdchg',k,PARAM.battery.num_batt,'LowerBound',0,'UpperBound',1,'Type','integer');
+    Pchg =      optimvar('Pchg',k,PARAM.battery.num_batt,'LowerBound',0,'UpperBound',inf);
+    xchg =      optimvar('xchg',k,PARAM.battery.num_batt,'LowerBound',0,'UpperBound',1,'Type','integer');
+    soc =       optimvar('soc',k+1,PARAM.battery.num_batt,'LowerBound',ones(k+1,PARAM.battery.num_batt).*PARAM.battery.min,'UpperBound',ones(k+1,PARAM.battery.num_batt).*PARAM.battery.max);
     prob =      optimproblem('Objective',sum(u) );
     
     %constraint part
@@ -70,32 +68,30 @@ for i = 1:length(dataset_name)
     prob.Constraints.epicons1 = -PARAM.Resolution*PARAM.Buy_rate.*Pnet - u <= 0;
     
     %--battery constraint
-    prob.Constraints.chargeconsbatt1 = Pchg(:,1)  <= xchg(:,1)*PARAM.battery.charge_rate;
-    prob.Constraints.chargeconsbatt2 = Pchg(:,2)  <= xchg(:,2)*PARAM.battery.charge_rate;
+
+    prob.Constraints.chargeconsbatt = Pchg <= xchg.*(ones(k,PARAM.battery.num_batt).*PARAM.battery.charge_rate);
     
-    prob.Constraints.dischargeconsbatt1 = Pdchg(:,1)   <= xdchg(:,1) *PARAM.battery.discharge_rate;
-    prob.Constraints.dischargeconsbatt2 = Pdchg(:,2)   <= xdchg(:,2) *PARAM.battery.discharge_rate;
+    prob.Constraints.dischargeconsbatt = Pdchg   <= xdchg.*(ones(k,PARAM.battery.num_batt).*PARAM.battery.discharge_rate);
     
-    prob.Constraints.NosimultDchgAndChgbatt1 = xchg(:,1) + xdchg(:,1) >= 0;
-    prob.Constraints.NosimultDchgAndChgbatt2 = xchg(:,2) + xdchg(:,2) >= 0;
+    prob.Constraints.NosimultDchgAndChgbatt = xchg + xdchg >= 0;
     
-    prob.Constraints.NosimultDchgAndChgconsbatt1 = xchg(:,1) + xdchg(:,1) <= 1;
-    prob.Constraints.NosimultDchgAndChgconsbatt2 = xchg(:,2) + xdchg(:,2) <= 1;
+    prob.Constraints.NosimultDchgAndChgconsbatt1 = xchg + xdchg <= 1;
+    
     %--Pnet constraint
-    prob.Constraints.powercons = Pnet == PARAM.PV + Pdchg(:,1) + Pdchg(:,2) - PARAM.PL - Pchg(:,1) - Pchg(:,2);
+    prob.Constraints.powercons = Pnet == PARAM.PV + sum(Pdchg,2) - PARAM.PL - sum(Pchg,2);
     
     %end of static constraint part
     
     %--soc dynamic constraint 
-    soccons = optimconstr(k+1,2);
-    soccons(1,1:2) = soc(1,1:2)  == PARAM.battery.initial ;
-    soccons(2:k+1,1) = soc(2:k+1,1)  == soc(1:k,1) + ...
-                             (PARAM.battery.charge_effiency*100*PARAM.Resolution/PARAM.battery.actual_capacity)*Pchg(1:k,1) ...
-                                - (PARAM.Resolution*100/(PARAM.battery.discharge_effiency*PARAM.battery.actual_capacity))*Pdchg(1:k,1);
-    soccons(2:k+1,2) = soc(2:k+1,2)  == soc(1:k,2) + ...
-                             (PARAM.battery.charge_effiency*100*PARAM.Resolution/PARAM.battery.actual_capacity)*Pchg(1:k,2) ...
-                                - (PARAM.Resolution*100/(PARAM.battery.discharge_effiency*PARAM.battery.actual_capacity))*Pdchg(1:k,2);
+    soccons = optimconstr(k+1,PARAM.battery.num_batt);
     
+    soccons(1,1:PARAM.battery.num_batt) = soc(1,1:PARAM.battery.num_batt)  == PARAM.battery.initial ;
+    for i = 1:PARAM.battery.num_batt
+        soccons(2:k+1,i) = soc(2:k+1,i)  == soc(1:k,i) + ...
+                                 (PARAM.battery.charge_effiency(:,i)*100*PARAM.Resolution/PARAM.battery.actual_capacity(:,i))*Pchg(1:k,i) ...
+                                    - (PARAM.Resolution*100/(PARAM.battery.discharge_effiency(:,i)*PARAM.battery.actual_capacity(:,i)))*Pdchg(1:k,i);
+        
+    end
     prob.Constraints.soccons = soccons;
     
     
@@ -105,5 +101,5 @@ for i = 1:length(dataset_name)
     sol = solve(prob,'Options',options);
     sol.dataset_name = dataset_name{i};
     sol.PARAM = PARAM;
-    save(strcat('solution/EMS1/',TOU_CHOICE,'_',dataset_name{i},'.mat'),'-struct','sol')
+    save(strcat('solution/EMS1/2batt/',TOU_CHOICE,'_',dataset_name{i},'.mat'),'-struct','sol')
 end
